53222a30a246dad8abd2dac4f3f86d3b150a884d
tavisca-amitesh <amitesh.kumar@tavisca.com>
2021-08-11 12:38:43 +0530

    Adding cognito config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-2_g9TKa56Xz', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3ftlbnei32i9vjvetq6qdf9q4v', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
